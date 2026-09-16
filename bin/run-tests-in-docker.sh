ee6770806a13d0f65788ea1d820dc6e4d823ad42
Aurélien Geron <ageron@users.noreply.github.com>
2026-09-17 09:46:30 +1200

    ci: test cached amd64 Docker image

--- bin/run-tests-in-docker.sh
+++ bin/run-tests-in-docker.sh
@@ -15,8 +15,13 @@
 # Stop executing when a command returns a non-zero return code
 set -e
 
-# Build the AMD64 Docker image used by Exercism production.
-docker build --platform linux/amd64 -t exercism/roc-test-runner .
+image="${TEST_RUNNER_IMAGE:-exercism/roc-test-runner}"
+
+# Build the AMD64 Docker image used by Exercism production. CI supplies an
+# already-built, cached image and skips this step.
+if [ "${SKIP_DOCKER_BUILD:-0}" != "1" ]; then
+    docker build --platform linux/amd64 -t "${image}" .
+fi
 
 # Run the Docker image using the settings mimicking the production environment
 docker run \
@@ -29,4 +34,4 @@ docker run \
     --volume "${PWD}/bin/run-tests.sh:/opt/test-runner/bin/run-tests.sh" \
     --workdir /opt/test-runner \
     --entrypoint /opt/test-runner/bin/run-tests.sh \
-    exercism/roc-test-runner
+    "${image}"
